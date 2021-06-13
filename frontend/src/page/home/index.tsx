fbb0f64d1983269170fbb471e14ce85e624917bb
allblack <umh0811@naver.com>
2021-06-13 20:21:58 +0900

    update

--- frontend/src/page/home/index.tsx
+++ frontend/src/page/home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import Layout from "../../components/Layout";
 import UseStore from "../../store";
 
@@ -7,8 +8,28 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  line-height: 1.8rem;
+  & > a {
+    position: relative;
+    &::before {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 0px;
+      height: 3px;
+      background: black;
+      content: "";
+      transition: width 0.5s;
+    }
+    &:hover {
+      &::before {
+        width: 100%;
+      }
+    }
+  }
 `;
 
 function Home() {
@@ -25,6 +46,7 @@ function Home() {
       <Layout>
         <Wrapper>
           <div>로그인 후 이용해주세요</div>
+          <Link to="/login">로그인하러 가기</Link>
         </Wrapper>
       </Layout>
     );
