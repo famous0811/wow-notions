ef81ba6b09ad0b4d22781cbd6c94fe94315c2413
allblack <umh0811@naver.com>
2021-06-14 12:56:17 +0900

    draft-js toolbar setting

--- frontend/src/page/home/index.tsx
+++ frontend/src/page/home/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Layout from "../../components/Layout";
 import UseStore from "../../store";
 
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState } from "draft-js";
+
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
-  display: flex;
+  /* display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: center; */
   line-height: 1.8rem;
   & > a {
     position: relative;
@@ -30,14 +34,73 @@ const Wrapper = styled.div`
       }
     }
   }
+  .toolbar-class {
+    position: fixed;
+    top: 50%;
+    left: 0;
+    /* width: 100px; */
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    overflow: hidden;
+    transition: height 0.5s, width 0.55s, border-radius 0.5s;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.1);
+    /* border-color: red; */
+    &:hover {
+      width: 300px;
+      height: 70px;
+      border-radius: 10px;
+    }
+  }
+  .editor {
+    height: calc(100vh - 54px);
+  }
+  .wrapper-class {
+    width: 100%;
+    /* height: 100%; */
+    max-width: 80%;
+    margin: 0 auto;
+    text-align: center;
+  }
 `;
 
 function Home() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const onEditorStateChange = (editorState: any) => {
+    // editorState에 값 설정
+    console.log(editorState);
+    setEditorState(editorState);
+  };
   if (UseStore.UserStore.token)
     return (
       <Layout>
         <Wrapper>
-          <div>로그인 완료</div>
+          {/* <div>로그인 완료</div> */}
+          <Editor
+            // 에디터와 툴바 모두에 적용되는 클래스
+            wrapperClassName="wrapper-class"
+            // 에디터 주변에 적용된 클래스
+            editorClassName="editor"
+            // 툴바 주위에 적용된 클래스
+            toolbarClassName="toolbar-class"
+            // 툴바 설정
+            toolbar={{
+              // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
+              list: { inDropdown: true },
+              textAlign: { inDropdown: true },
+              link: { inDropdown: true },
+              history: { inDropdown: false },
+            }}
+            placeholder="내용을 작성해주세요."
+            // 한국어 설정
+            localization={{
+              locale: "ko",
+            }}
+            // 초기값 설정
+            editorState={editorState}
+            // 에디터의 값이 변경될 때마다 onEditorStateChange 호출
+            onEditorStateChange={onEditorStateChange}
+          />
         </Wrapper>
       </Layout>
     );
@@ -46,7 +109,6 @@ function Home() {
       <Layout>
         <Wrapper>
           <div>로그인 후 이용해주세요</div>
-          <Link to="/login">로그인하러 가기</Link>
         </Wrapper>
       </Layout>
     );
