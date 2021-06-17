ab18eeb2dc84551ea0bf2201a5af851ae8119a90
allblack <umh0811@naver.com>
2021-06-17 11:48:51 +0900

    toolbar desain setting

--- frontend/src/components/ui/Toolbar.tsx
+++ frontend/src/components/ui/Toolbar.tsx
@@ -2,62 +2,89 @@ import React, { useState } from "react";
 import { EditorState } from "draft-js";
 import styled, { css } from "styled-components";
 
+const toolbar_box = css`
+  width: 400px;
+  height: 120px;
+  border-radius: 10px;
+  overflow: visible;
+  /* baise style */
+`;
+const toolbar_drag_box = css`
+  width: 100%;
+  height: 20px;
+  display: flex !important;
+  justify-content: center;
+  cursor: pointer;
+`;
+
 const ToolbarWrap = styled.div<{ Toolfixing: boolean }>`
-  &.toolbar_class {
-    position: fixed;
-    top: 50%;
-    left: 0;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    overflow: hidden;
-    transition: height 0.7s, width 0.75s, border-radius 0.5s;
-    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.1);
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-between;
-    align-items: center;
-    z-index: 20;
-    &:hover {
-      width: 400px;
-      height: 120px;
-      border-radius: 10px;
-      overflow: visible;
-      & > .toolbar_drag {
-        width: 100%;
-        height: 20px;
-        display: flex;
-        justify-content: center;
-        cursor: pointer;
-      }
+  //before hover & click
+  position: fixed;
+  top: 50%;
+  left: 0;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  overflow: hidden;
+  transition: height 0.7s, width 0.75s, border-radius 0.5s;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.1);
+  padding: 15px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.4rem;
+  z-index: 20;
+
+  &:hover {
+    ${toolbar_box}
+
+    &>.toolbar_drag {
+      ${toolbar_drag_box};
     }
-    ${(props) =>
-      props.Toolfixing &&
-      css`
-        width: 400px;
-        height: 120px;
-        border-radius: 10px;
-        overflow: visible;
-      `}
+    & > .toolbar_item {
+      display: flex;
+    }
   }
+  ${(props) => props.Toolfixing && toolbar_box}
+
+  //drage button desain
   & > .toolbar_drag {
     display: none;
+    background: rgba(0, 0, 0, 0.1);
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    border-radius: 0px 0px 10px 10px;
+    ${(props) => props.Toolfixing && toolbar_drag_box}
+    //material icon setting
+      & > span {
+      margin-top: -10px;
+    }
+  }
+
+  //functions button desain
+  & > .toolbar_item {
     ${(props) =>
       props.Toolfixing &&
       css`
-        width: 100%;
-        height: 20px;
-        display: flex;
-        justify-content: center;
-        cursor: pointer;
+        display: flex !important;
       `}
-  }
-  & > .toolbar_item {
     border: none;
+    display: none;
     border-radius: 10px;
-    display: flex;
+    padding: 5px 10px;
+    margin: 0 2px;
     justify-content: center;
     align-items: center;
+    transition: all 0.5s;
+
+    &:hover {
+      background-color: white;
+    }
+    &:nth-child(1) {
+      display: flex;
+    }
   }
 `;
 
@@ -100,24 +127,44 @@ const Toolbar: React.FC<ToolbarProps> = ({
       >
         h3
       </button>
-      <button onMouseDown={(e) => handleBlockClick(e, "unstyled")}>
+      <button
+        onMouseDown={(e) => handleBlockClick(e, "unstyled")}
+        className="toolbar_item"
+      >
         normal
       </button>
 
-      <button onMouseDown={(e) => handleTogggleClick(e, "BOLD")}>bold</button>
-      <button onMouseDown={(e) => handleTogggleClick(e, "ITALIC")}>
-        italic
-      </button>
-      <button onMouseDown={(e) => handleTogggleClick(e, "STRIKETHROUGH")}>
-        strikthrough
-      </button>
-      <button onMouseDown={(e) => handleBlockClick(e, "ordered-list-item")}>
-        ol
-      </button>
-      <button onMouseDown={(e) => handleBlockClick(e, "unordered-list-item")}>
-        ul
+      <button
+        onMouseDown={(e) => handleTogggleClick(e, "BOLD")}
+        className="toolbar_item"
+      >
+        bold
       </button>
       <button
+        onMouseDown={(e) => handleTogggleClick(e, "ITALIC")}
+        className="toolbar_item"
+      >
+        italic
+      </button>
+      <button
+        onMouseDown={(e) => handleTogggleClick(e, "STRIKETHROUGH")}
+        className="toolbar_item"
+      >
+        strikthrough
+      </button>
+      <button
+        onMouseDown={(e) => handleBlockClick(e, "ordered-list-item")}
+        className="toolbar_item"
+      >
+        ol
+      </button>
+      <button
+        onMouseDown={(e) => handleBlockClick(e, "unordered-list-item")}
+        className="toolbar_item"
+      >
+        ul
+      </button>
+      {/* <button
         disabled={editorState.getUndoStack().size <= 0}
         onMouseDown={() => setEditorState(EditorState.undo(editorState))}
       >
@@ -128,7 +175,7 @@ const Toolbar: React.FC<ToolbarProps> = ({
         onMouseDown={() => setEditorState(EditorState.redo(editorState))}
       >
         redo
-      </button>
+      </button> */}
       <div className="toolbar_drag">
         <span className="material-icons">minimize</span>
       </div>
